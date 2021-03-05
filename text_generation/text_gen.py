efd69a2e56092d5c43cbca93ce0b7c72597f191f
tiagosousagarcia <tiagosousagarcia@gmail.com>
2021-03-05 15:44:29 +0000

    Working text generation

--- text_generation/text_gen.py
+++ text_generation/text_gen.py
@@ -1,3 +1,25 @@
 #Example of text generation using GPT-2 model
 #Guide at https://towardsdatascience.com/text-generation-with-python-and-gpt-2-1fecbff1635b
 
+#See https://pytorch.org/
+import torch
+#See https://huggingface.co/transformers/index.html
+from transformers import GPT2LMHeadModel, GPT2Tokenizer
+
+#initialise tokeniser and model from pre-trained GPT2 model
+
+tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+model = GPT2LMHeadModel.from_pretrained('gpt2')
+
+#add input and tokenise it
+sequence = 'There once was a man from Nantucket'
+inputs = tokenizer.encode(sequence, return_tensors = 'pt')
+
+#generate -- output is a series of indices
+outputs = model.generate(inputs, max_length = 50, do_sample = True, temperature = .7, top_k = 50)
+
+#decode indices into words
+text = tokenizer.decode(outputs[0], skip_special_tokens = True)
+
+#print result
+print(text)
