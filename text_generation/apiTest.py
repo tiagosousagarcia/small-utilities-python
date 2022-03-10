9847710ef4bc8ff5dd1a25f2719c779f4d5337b4
Tiago Sousa Garcia <tiagosousagarcia@gmail.com>
2022-03-10 09:50:57 +0000

    working at basic level

---
+++ text_generation/apiTest.py
@@ -0,0 +1,57 @@
+import requests, time, logging, sys
+# API_ENDPOINT = "http://localhost:7071/api/orchestrators/orchestrator-generation-test"
+
+def send_text(prompt, API_ENDPOINT):
+    logging.info("Sending prompt")
+    message = {"prompt": prompt}
+    response = requests.post(API_ENDPOINT, json=message)
+    return response
+
+def get_links(response):
+    body = response.json()
+    # 'id'
+    # 'statusQueryGetUri'
+    # 'sendEventPostUri'
+    # 'terminatePostUri'
+    # 'rewindPostUri'
+    # 'purgeHistoryDeleteUri'
+    # 'restartPostUri'
+    logging.info(f"Request Id: {body['id']}")
+    return body['id'], body['statusQueryGetUri'], body['terminatePostUri']
+
+def check_status(status):
+    logging.info("Checking status")
+    status_response = requests.get(status)
+    body = status_response.json()
+    if status_response.status_code == 202 and body['runtimeStatus'] == "Running" or body['runtimeStatus'] == "Pending":
+        logging.info(f"Process still {body['runtimeStatus']}, trying again in 10 seconds")
+        time.sleep(10)
+        return check_status(status)
+    elif status_response.status_code == 200 and body['runtimeStatus'] == "Completed":
+        logging.info("Process completed, processing result")
+        logging.info(f"Instance Id: {body['instanceId']}")
+        logging.info(f"Output: {body['output']}")
+        return body['output'], status_response.status_code
+    else:
+        logging.error("Return code or runtimeStatus not expected")
+        return status_response.status_code
+
+
+
+# Logging setup
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s] %(message)s", handlers=[logging.FileHandler("API-debug.log"), logging.StreamHandler(sys.stdout)])
+
+# Main
+
+def start_generation(prompt, API_ENDPOINT):
+    reply_result = ""
+    response = send_text(prompt, API_ENDPOINT)
+    logging.info(f"Return code {response.status_code}")
+    if response.status_code == 202:
+        id_no, status, terminate = get_links(response)
+        reply_result, final_status = check_status(status)
+    return reply_result, final_status
+
+
+    
--- text_generation/app.py
+++ text_generation/app.py
@@ -1,9 +1,14 @@
 from operator import ge
 from flask import Flask, render_template, request
-from utilities import send_text
+# from utilities import send_text
+from apiTest import start_generation
+import logging, sys
 
-# POSTING to the azure functions is working, need to implement
-# the waiting for response methods
+# App is now working at a basic level. Need to implement loading screen
+
+# Logging setup
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s] %(message)s", handlers=[logging.FileHandler("app-debug.log"), logging.StreamHandler(sys.stdout)])
 
 API_ENDPOINT = "http://localhost:7071/api/orchestrators/orchestrator-generation-test"
 
@@ -11,19 +16,21 @@ app = Flask(__name__)
 
 @app.route('/', methods = ['POST', 'GET'])
 def home():
-    result = "Start typing your story"
+    response = "Start typing your story"
     if request.method == 'POST':
         prompt = request.form['line']
         try:
             # result = generate_text(prompt, 50)
-            response = send_text(prompt, API_ENDPOINT)
-            # app.logger.info("Response: ", response.status_code())
+            response, status = start_generation(prompt, API_ENDPOINT)
+            if response == "":
+                app.logger.info("Response: ", status)
+                response = "I'm sorry, something went wrong"
             pass
         except:
             return "There has been an error"
     else:
         pass
-    return render_template("index.html", content = result)
+    return render_template("index.html", content = response)
 
 if __name__ == "__main__":
     app.run(debug=True)
